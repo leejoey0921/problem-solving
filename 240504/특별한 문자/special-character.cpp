f512de2007ba07d61b5d4837bdcd5b3dbfd73580
Joey Lee <leejoey0921@gmail.com>
2024-05-04 23:32:21 +0900

    Title: 특별한 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240504/특별한 문자/special-character.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    
+    string s; cin >> s;
+    unordered_map<char, int> count, order;
+
+    for (int i = 0; i < s.length(); i++) {
+        count[s[i]]++;
+        if (order.find(s[i]) != order.end()) {
+            order[s[i]] = i;
+        }
+    }
+
+    int min_order = 1e5;
+    char ans;
+
+    for (auto [c, x] : count) {
+        int o = order[c];
+        if (x == 1 && o < min_order) {
+            min_order = o;
+            ans = c;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
