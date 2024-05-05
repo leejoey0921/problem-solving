e908bfba41e1d294c7b5ddc62f2199becdfb9255
Joey Lee <leejoey0921@gmail.com>
2024-05-05 15:29:53 +0900

    Title: 세 수의 합, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

--- 240505/세 수의 합/sum-of-three-num.cpp
+++ 240505/세 수의 합/sum-of-three-num.cpp
@@ -1,41 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k;
-unordered_map<int, int> freq;
-
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
-    cin >> n >> k;
+
+    int n, K;
+    cin >> n >> K;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
     int ans = 0;
+    unordered_map<int, int> mp;
+    for (int j = 0; j < n; j++) {
+        for (int k = j + 1; k < n; k++) {
+            // K - a[j] - a[k] 랑 같은 i의 개수
 
-    for(int i = 0; i < n; i++) {
-        int it; cin >> it;
-        freq[it]++;
-    }
-
-
-    for (auto i = freq.begin(); i != freq.end(); i++) {
-        for (auto j = next(i); j != freq.end(); j++) {
-            int k1, v1, k2, v2;
-            k1 = (*i).first; k2 = (*j).first; v1 = (*i).second; v2 = (*j).second;
-
-            int diff = k - k1 - k2;
-
-            cout << ans << "\n";
-            cout << k1 << " " << k2 << " " << diff << "\n";
-            if (diff == k1) {
-                ans += v1 * (v1-1) / 2 * v2;
-            } else if (diff == k2) {
-                ans += v2 * (v2-1) / 2 * v1;
-            } else if (freq[diff] > 0 && freq.find(diff) != freq.end()) {
-                ans += v1 * v2 * freq[diff];
+            if (mp.find(K - a[j] - a[k]) != mp.end()) {
+                ans += mp[K - a[j] - a[k]];
             }
         }
+        mp[a[j]]++;
     }
-
-    cout << ans;
-
-    return 0;
+    cout << ans << '\n';
 }
