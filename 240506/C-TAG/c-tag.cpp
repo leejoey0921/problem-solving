33c4d405b42ad12102032169f9e11004be412409
Joey Lee <leejoey0921@gmail.com>
2024-05-06 19:32:14 +0900

    Title: C-TAG, Time: 1293ms, Memory: 0MB, Status: Passed - CodeTree

--- 240506/C-TAG/c-tag.cpp
+++ 240506/C-TAG/c-tag.cpp
@@ -18,23 +18,23 @@ int main() {
     for (int i = 0; i < m; i++) {
         for (int j = i+1; j < m; j++) {
             for (int k = j+1; k < m; k++) {
-                unordered_set<string> a_tags, b_tags;
+                unordered_set<string> a_tags;
                 for (int l = 0; l < n; l++) {
                     string a_tag = a[l].substr(i, 1) + a[l].substr(j, 1) + a[l].substr(k, 1);
-                    string b_tag = b[l].substr(i, 1) + b[l].substr(j, 1) + b[l].substr(k, 1); 
                     a_tags.insert(a_tag);
-                    b_tags.insert(b_tag);
+                    
                 }
+
+                string b_tag;
                 bool valid = true;
-                for (auto a_tag : a_tags) {
-                    if (b_tags.find(a_tag) != b_tags.end()) {
+                for (int l = 0; l < n; l++) {
+                    b_tag = b[l].substr(i, 1) + b[l].substr(j, 1) + b[l].substr(k, 1); 
+                    if (a_tags.find(b_tag) != a_tags.end()) {
                         valid = false;
                         break;
                     }
                 }
-                if (valid) {
-                    ans++;
-                }
+                if (valid) ans++;
             }
         }
     }
