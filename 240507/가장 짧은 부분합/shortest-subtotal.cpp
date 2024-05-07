5e330a304bfcd3e88a14f378918e5a5864f744eb
Joey Lee <leejoey0921@gmail.com>
2024-05-07 21:23:11 +0900

    Title: 가장 짧은 부분합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240507/가장 짧은 부분합/shortest-subtotal.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, s; cin >> n >> s;
+
+    int arr[n];
+
+    for (int i; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = arr[0];
+    int ans = 1e8;
+    for (int i; i < n; i++) {
+        for (int j = 1; j < n && sum < s; j++) {
+            sum += j;
+            ans = min(j - i + 1, ans);
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
