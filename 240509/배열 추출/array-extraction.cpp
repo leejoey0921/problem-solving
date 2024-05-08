e87855a109394c2cda5a09928153575ff8337e45
Joey Lee <leejoey0921@gmail.com>
2024-05-09 00:15:21 +0900

    Title: 배열 추출, Time: 38ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240509/배열 추출/array-extraction.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+
+    int n; cin >> n;
+    priority_queue<int> pq;
+    
+    for (int i; i < n; i++) {
+        int x; cin >> x;
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << "0\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+
+    return 0;
+}
