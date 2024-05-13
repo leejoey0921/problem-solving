1b1ecf51314af6a308c8fdc62dce607d60768b7b
Joey Lee <leejoey0921@gmail.com>
2024-05-13 21:41:53 +0900

    Title: 연속한 K개의 숫자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240513/연속한 K개의 숫자/k-numbers-in-a-row.cpp
+++ 240513/연속한 K개의 숫자/k-numbers-in-a-row.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
     int n, k, b; cin >> n >> k >> b;
-    int arr[n];
-    fill_n(arr, n, 1);
+    int arr[n] = {};
 
     for (int i; i < b; i++) {
         int x; cin >> x;
-        arr[x-1] = 0;
+        arr[x-1] = 1;
     }
 
     int count = 0;
@@ -17,11 +16,11 @@ int main() {
         count += arr[i];
     }
 
-    int ans = k - count;
+    int ans = count;
 
     for (int i; i <= n-k; i++) {
-        count = count - arr[i] + arr[i+k-1];
-        ans = min(ans, k - count);
+        count = count - arr[i] + arr[i+k];
+        ans = min(ans, count);
     }
     cout << ans;
 
