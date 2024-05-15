c2b01f2da7f4725b0358500c0a7ca1471941a8b9
Joey Lee <leejoey0921@gmail.com>
2024-05-15 22:21:02 +0900

    Title: 최대 거리의 점, Time: 72ms, Memory: 1MB, Status: Passed - CodeTree

--- 240515/최대 거리의 점/maximum-distance-point.cpp
+++ 240515/최대 거리의 점/maximum-distance-point.cpp
@@ -6,24 +6,24 @@ typedef long long ll;
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
 
-    int n, m; cin >> n >> m;
+    ll n, m; cin >> n >> m;
 
     ll points[n];
 
-    for (int i; i < n; i++) {
+    for (ll i; i < n; i++) {
         cin >> points[i];
     }
     sort(points, points + n);
 
     // 물건 간의 최소 간격으로 이분탐색
-    int l = 0; int r = 1e9;
+    ll l = 0; ll r = 1e9;
 
-    int ans;
-    while (l < r) {
-        int mid = (l + r) / 2;
+    ll ans = 0;
+    while (l <= r) {
+        ll mid = (l + r) / 2;
 
         ll start = points[0];
-        int count = 1;
+        ll count = 1;
         for (auto p : points) {
             if (p - start >= mid) {
                 count++;
@@ -33,8 +33,9 @@ int main() {
         // cout << l << " " << r << " " << mid << " " << count << " " << endl;
 
         if (count >= m) { // mid <= ans
-            l = mid;
             ans = max(ans, mid);
+            // if (l == r - 1 && l == mid) break;
+            l = mid + 1;
         } else { // mid > ans
             r = mid - 1;
         }
