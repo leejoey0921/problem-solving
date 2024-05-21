c2d9c1f25c2594707dff82a717ba7e7f0798d81f
Joey Lee <leejoey0921@gmail.com>
2024-05-22 00:12:49 +0900

    Title: 적석과 흑석 2, Time: 73ms, Memory: 5MB, Status: Passed - CodeTree

---
+++ 240522/적석과 흑석 2/red-stone-and-black-stone-2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    set<int> Red;
+    vector<pii> Black;
+
+    int c, n; cin >> c >> n;
+
+    for (int i = 0; i < c; i++) {
+        int x; cin >> x;
+        Red.insert(x);
+    }
+
+    for (int i = 0; i < n; i++) {
+        int a, b; cin >> a >> b;
+        Black.push_back({b, a});
+    }
+
+    sort(Black.begin(), Black.end());
+
+    int ans = 0;
+    for (auto [e, s] : Black) {
+        auto q = Red.lower_bound(s);
+        if (q != Red.end() && *q <= e) {
+            ans++;
+            Red.erase(q);
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
