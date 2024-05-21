7fab30891c04abdbf027745c6a871ac49b481f0d
Joey Lee <leejoey0921@gmail.com>
2024-05-21 20:19:34 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 54ms, Memory: 2MB, Status: Passed - CodeTree

--- 240521/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
+++ 240521/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// 변수 선언
-int n, m;
-vector<tuple<double, int, int> > jewels;
-double ans;
+typedef pair<double, double> pdd;
 
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
-    // 입력:
-    cin >> n >> m;
-    for(int i = 0; i < n; i++) {
-        int w, v;
-        cin >> w >> v;
-        jewels.push_back(make_tuple(-(double)v / w, w, v));
+    double n, m; cin >> n >> m;
+    vector<pdd> gems;
+
+    for (int i = 0; i < n; i++) {
+        double w, v; cin >> w >> v;
+        gems.push_back({w, v});
     }
 
-    // 가치 / 무게가 내림차순이 되도록 정렬합니다.
-    sort(jewels.begin(), jewels.end());
+    sort(gems.begin(), gems.end(), [&](pdd p, pdd q) {
+        return (p.first / p.second) < (q.first / q.second);
+    });
 
-    for(int i = 0; i < n; i++) {
-        int w, v;
-        tie(ignore, w, v) = jewels[i];
-        // 현재 보석을 온전히 다 담을 수 있다면 그대로 담아줍니다.
-        if(m >= w) {
-            m -= w;
-            ans += v;
-        }
-        // 만약 부분만 담을 수 있다면
-        // 비율에 맞춰 담아준 뒤 종료합니다.
-        else {
-            ans += (double)m / w * v;
+    double acc_v = 0;
+    double acc_w = 0;
+
+    for (auto [w, v] : gems) {
+        if (acc_w + w < m) {
+            acc_v += v;
+            acc_w += w;
+        } else {
+            acc_v += (m - acc_w) * ((double) v / (double) w);
             break;
         }
     }
-
     cout << fixed;
     cout.precision(3);
-    cout << ans;
+    cout << acc_v;
+
     return 0;
 }
