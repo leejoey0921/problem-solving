9dd94f4af67bb7ae28c2c29634f0822ba2ee087a
Joey Lee <leejoey0921@gmail.com>
2024-05-21 20:18:35 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 29ms, Memory: 2MB, Status: Passed - CodeTree

--- 240521/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
+++ 240521/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
@@ -1,37 +1,43 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-typedef pair<float, float> pff;
+// 변수 선언
+int n, m;
+vector<tuple<double, int, int> > jewels;
+double ans;
 
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
-    float n, m; cin >> n >> m;
-    vector<pff> gems;
-
-    for (int i = 0; i < n; i++) {
-        float w, v; cin >> w >> v;
-        gems.push_back({w, v});
+    // 입력:
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        int w, v;
+        cin >> w >> v;
+        jewels.push_back(make_tuple(-(double)v / w, w, v));
     }
 
-    sort(gems.begin(), gems.end(), [&](pff p, pff q) {
-        return (p.first / p.second) < (q.first / q.second);
-    });
+    // 가치 / 무게가 내림차순이 되도록 정렬합니다.
+    sort(jewels.begin(), jewels.end());
 
-    float acc_v = 0;
-    float acc_w = 0;
-
-    for (auto [w, v] : gems) {
-        if (acc_w + w < m) {
-            acc_v += v;
-            acc_w += w;
-        } else {
-            acc_v += (m - acc_w) * (v / w);
+    for(int i = 0; i < n; i++) {
+        int w, v;
+        tie(ignore, w, v) = jewels[i];
+        // 현재 보석을 온전히 다 담을 수 있다면 그대로 담아줍니다.
+        if(m >= w) {
+            m -= w;
+            ans += v;
+        }
+        // 만약 부분만 담을 수 있다면
+        // 비율에 맞춰 담아준 뒤 종료합니다.
+        else {
+            ans += (double)m / w * v;
             break;
         }
     }
+
     cout << fixed;
     cout.precision(3);
-    cout << acc_v;
-
+    cout << ans;
     return 0;
 }
