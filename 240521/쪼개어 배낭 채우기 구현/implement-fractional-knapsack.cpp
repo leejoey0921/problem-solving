13a528feebce55d2742e501c6c79d6906afb95db
Joey Lee <leejoey0921@gmail.com>
2024-05-21 20:16:27 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 34ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240521/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<float, float> pff;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    float n, m; cin >> n >> m;
+    vector<pff> gems;
+
+    for (int i = 0; i < n; i++) {
+        float w, v; cin >> w >> v;
+        gems.push_back({w, v});
+    }
+
+    sort(gems.begin(), gems.end(), [&](pff p, pff q) {
+        return (p.first / p.second) < (q.first / q.second);
+    });
+
+    float acc_v = 0;
+    float acc_w = 0;
+
+    for (auto [w, v] : gems) {
+        if (acc_w + w < m) {
+            acc_v += v;
+            acc_w += w;
+        } else {
+            acc_v += (m - acc_w) * (v / w);
+            break;
+        }
+    }
+    cout << fixed;
+    cout.precision(3);
+    cout << acc_v;
+
+    return 0;
+}
