45c7d932b1496179d6aef207fdfb77ffdaeb2840
Joey Lee <leejoey0921@gmail.com>
2024-06-06 16:49:47 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 231ms, Memory: 4MB, Status: Passed - CodeTree

--- 240606/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.cpp
+++ 240606/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.cpp
@@ -2,33 +2,42 @@
 using namespace std;
 
 typedef long long ll;
+typedef pair<int, int> pii;
 
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
 
     int n; cin >> n;
     vector<string> seqs;
-    vector<pair<ll, string>> scores;
+    map<string, pii> cnts;
+
     for (int i = 0; i < n; i++) {
         string s; cin >> s;
         seqs.push_back(s);
     }
 
-    for (string seq: seqs) {
-        ll score = 0;
-        ll cur = 5e5;
-        for (char c : seq) {
-            if (c == '(') score += cur;
-            else score -= cur;
-            cur--;
+    for (string s : seqs) {
+        int open = 0; int close = 0;
+        for (char c : s) {
+            if (c == '(') open++;
+            else close++;
         }
-        scores.push_back({-score, seq});
+        cnts[s] = {open, close};
     }
-    sort(scores.begin(), scores.end());
+
+    sort(
+        seqs.begin(),
+        seqs.end(),
+        [&](string s1, string s2) {
+            auto [open1, close1] = cnts[s1];
+            auto [open2, close2] = cnts[s2];
+            return open1 * close2 > open2 * close1;
+        }
+    );
 
     string res_seq = "";
-    for (auto [score, seq] : scores) {
-        res_seq += seq;
+    for (string s : seqs) {
+        res_seq += s;
     }
 
     ll open_cnt = 0;
@@ -38,7 +47,7 @@ int main() {
         else ans += open_cnt;
     }
 
-    cout << ans;
+    cout << ans;   
 
     return 0;
 }
