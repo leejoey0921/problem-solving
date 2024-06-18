4b62294d1f2f10cb8dac1eaad92ca8c8100a1774
Joey Lee <leejoey0921@gmail.com>
2024-06-18 22:27:09 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = """"
+                 "\\    /\\\n"
+               " )  ( ')\n"
+               "(  /  )\n"
+               " \\(__)|""";
+    cout << s;
+    return 0;
+}
